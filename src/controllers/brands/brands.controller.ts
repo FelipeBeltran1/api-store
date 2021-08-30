cc9e3fcc5fef186aa975c6da392675bdb01323a4
Andres Cadena <andrescadena0607@gmail.com>
2021-08-30 09:54:34 -0500

    get brands - customers - orders - users

--- src/controllers/brands/brands.controller.ts
+++ src/controllers/brands/brands.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('brands')
-export class BrandsController {}
+export class BrandsController {
+  @Get()
+  getCustomers() {
+    return {
+      message: `customers`,
+    };
+  }
+}
--- src/controllers/customers/customers.controller.ts
+++ src/controllers/customers/customers.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+  @Get()
+  getCustomers() {
+    return {
+      message: `customers`,
+    };
+  }
+}
