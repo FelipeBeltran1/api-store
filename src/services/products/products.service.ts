4a8b5043889a6120e5873d54b5444e2a77bf5e8c
Andres Cadena <andrescadena0607@gmail.com>
2021-08-30 11:31:29 -0500

    entity product and service product update

--- src/services/products/products.service.ts
+++ src/services/products/products.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
-
+import { Product } from './../../entities/product.entity';
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private counterId = 1;
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'asd',
+      price: 122,
+      stock: 10,
+      image: '',
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  create(payload: any) {
+    this.counterId = this.counterId + 1;
+    const newProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+}
