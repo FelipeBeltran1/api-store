41f6aeed8261169ae4e73be32b54c189626a2c6f
Andres Cadena <andrescadena0607@gmail.com>
2021-08-30 10:39:54 -0500

    put and delete brand

--- src/controllers/brands/brands.controller.ts
+++ src/controllers/brands/brands.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+} from '@nestjs/common';
 
 @Controller('brands')
 export class BrandsController {
@@ -14,6 +22,21 @@ export class BrandsController {
     return {
       message: 'crear brand',
       payload,
+    };
+  }
+
+  @Put(':id')
+  updateBrand(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  deleteBrand(@Param('id') id: number) {
+    return {
+      message: `eliminado ${id}`,
     };
   }
 }
