aceaa1c7d412bdb504f033f442e12d00e83f5fa5
Andres Cadena <andrescadena0607@gmail.com>
2021-08-30 10:50:44 -0500

    put and delete order

--- src/controllers/orders/orders.controller.ts
+++ src/controllers/orders/orders.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('orders')
 export class OrdersController {
@@ -14,6 +22,21 @@ export class OrdersController {
     return {
       message: 'crear order',
       payload,
+    };
+  }
+
+  @Put(':id')
+  updateOrder(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  deleteOrder(@Param('id') id: number) {
+    return {
+      message: `eliminado ${id}`,
     };
   }
 }
