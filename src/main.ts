c7e9078a17530bed505987ecf36e983655786eae
Andres Cadena <andrescadena0607@gmail.com>
2021-09-22 11:48:56 -0500

    update packages

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
@@ -10,6 +11,13 @@ async function bootstrap() {
       forbidNonWhitelisted: true, //alertar
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Api Store')
+    .setDescription('Nestjs api example')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
