ac8418bd6048081609030dc9ddb1bd51f17d08df
Andres Cadena <andrescadena0607@gmail.com>
2021-09-29 10:06:57 -0500

    add docker and nodepostgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpModule, HttpService, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { Client } from 'pg';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,6 +10,19 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { enviroments } from './enviroments';
 import config from './config';
+
+const client = new Client({
+  user: 'root',
+  host: 'localhost',
+  database: 'my_db',
+  password: 'root',
+  port: 5432,
+});
+client.connect();
+client.query('SELECT * FROM tasks', (err, res) => {
+  console.error(err);
+  console.log(res.rows);
+});
 @Module({
   imports: [
     ConfigModule.forRoot({
