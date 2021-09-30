1de4d677335c50c4564d0559995e7c064c4a828c
Andres Cadena <andrescadena0607@gmail.com>
2021-09-30 09:54:47 -0500

    update user module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,13 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CustomersController } from './controllers/customers.controller';
 import { CustomersService } from './services/customers.service';
+import { Customer } from 'src/entities/users/customer.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './services/users.service';
-
-import { ProductsModule } from 'src/modules/products/products.module';
-import { Customer } from 'src/entities/users/customer.entity';
-import { Order } from 'src/entities/users/order.entity';
 import { User } from 'src/entities/users/user.entity';
+import { Order } from 'src/entities/users/order.entity';
+import { ProductsModule } from 'src/modules/products/products.module';
 
 @Module({
   imports: [
