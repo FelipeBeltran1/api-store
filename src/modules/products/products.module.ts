5789ae92f3e22ba3987d2dfa26aee3ebe1139244
Andres Cadena <andrescadena0607@gmail.com>
2021-09-30 09:53:48 -0500

    update product module

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProductsController } from './products.controller';
-import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
-import { BrandsService } from './services/brands.service';
-import { CategoriesService } from './services/categories.service';
-import { BrandsController } from './controllers/brands.controller';
 import { Product } from 'src/entities/products/product.entity';
+import { CategoriesController } from './controllers/categories.controller';
+import { CategoriesService } from './services/categories.service';
 import { Category } from 'src/entities/products/category.entity';
+import { BrandsController } from './controllers/brands.controller';
+import { BrandsService } from './services/brands.service';
 import { Brand } from 'src/entities/products/brand.entity';
 
 @Module({
