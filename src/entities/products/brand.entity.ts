22d6454d0e4e5958af9d52ec8476355105d5de07
Andres Cadena <andrescadena0607@gmail.com>
2021-10-02 15:03:01 -0500

    add: relationship between product and brand

--- src/entities/products/brand.entity.ts
+++ src/entities/products/brand.entity.ts
@@ -2,16 +2,18 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Product } from './product.entity';
 
 @Entity('brand', { schema: 'products' })
 export class Brand {
   @PrimaryGeneratedColumn({ type: 'bigint' })
   id: number;
 
-  @Column('character varying', { length: 200 })
+  @Column('character varying', { length: 200, unique: true })
   name: string;
 
   @Column('character varying', { length: 250 })
@@ -28,4 +30,7 @@ export class Brand {
     default: () => 'CURRENT_TIMESTAMP',
   })
   updateAt: Date;
+
+  @OneToMany(() => Product, (product) => product.brand)
+  products: Product[];
 }
