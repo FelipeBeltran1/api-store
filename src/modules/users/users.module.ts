3a51023cb1d09c7aa3811f3e09662aa890f1f18f
Andres Cadena <andrescadena0607@gmail.com>
2021-10-02 17:31:05 -0500

    add: orders controller and service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -10,13 +10,15 @@ import { User } from 'src/entities/users/user.entity';
 import { Order } from 'src/entities/users/order.entity';
 import { ProductsModule } from 'src/modules/products/products.module';
 import { OrderProduct } from 'src/entities/users/orderProduct.entity';
+import { OrdersService } from './services/orders.service';
+import { OrdersController } from './controllers/orders.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Customer, Order, OrderProduct], 'users'),
     ProductsModule,
   ],
-  controllers: [CustomersController, UsersController],
-  providers: [CustomersService, UsersService],
+  controllers: [CustomersController, UsersController, OrdersController],
+  providers: [CustomersService, UsersService, OrdersService],
 })
 export class UsersModule {}
