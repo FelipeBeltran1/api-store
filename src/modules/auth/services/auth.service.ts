404dc19fb21fb1098a1fa3eb10e1e5e39b744bdc
Andres Cadena <andrescadena0607@gmail.com>
2021-10-13 18:03:36 -0500

    add: passport jwt

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/modules/users/services/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/entities/users/user.entity';
+import { PayloadToken } from '../models/token.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private UserService: UsersService) {}
+  constructor(
+    private UserService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.UserService.findByEmail(email);
@@ -13,5 +19,13 @@ export class AuthService {
       if (isMatch) return user;
       return null;
     }
+  }
+
+  generateJWT(user: User) {
+    const payload: PayloadToken = { role: user.role, sub: user.id };
+    return {
+      acces_token: this.jwtService.sign(payload),
+      user,
+    };
   }
 }
