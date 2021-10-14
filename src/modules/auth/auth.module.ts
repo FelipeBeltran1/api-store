5de14972bb149fe339d4b0399a24c072a3aada76
Andres Cadena <andrescadena0607@gmail.com>
2021-10-14 10:02:48 -0500

    add: secret environments

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,22 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from 'src/@common/strategys/local.strategy';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './services/auth.service';
-import { AuthController } from './controllers/auth.controller';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_KEY'),
+        signOptions: { expiresIn: configService.get('JWT_EXPIRE') },
+      }),
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
