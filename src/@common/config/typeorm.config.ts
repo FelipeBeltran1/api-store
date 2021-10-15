14527a21fc21b6f92df2ec7478f3be360d246a76
Andres Cadena <andrescadena0607@gmail.com>
2021-10-15 11:26:49 -0500

    update typeormconfig

--- src/@common/config/typeorm.config.ts
+++ src/@common/config/typeorm.config.ts
@@ -3,7 +3,11 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('typeorm', () => {
   const configDefault = {
     type: process.env.DB_TYPE,
-    url: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: false,
     logging: false,
     entities: ['dist/entities/**/*entity{.ts,.js}'],
@@ -11,9 +15,6 @@ export default registerAs('typeorm', () => {
     migrations: ['dist/src/@common/migrations/*.ts'],
     cli: {
       migrationsDir: 'src/@common/migrations/migrations',
-    },
-    ssl: {
-      rejectUnauthorized: false,
     },
     apiKey: process.env.API_KEY,
     //entities: [(process.env.NODE_ENV === 'local' ? 'src/entities/**/*.ts' : 'dist/entities/**/*.js' )]
